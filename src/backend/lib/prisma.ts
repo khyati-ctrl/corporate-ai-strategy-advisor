ce893d0fe529c907b72586115e9ec74fa82a78e1
pjkorba1256-cmd <pj.korba1256@gmail.com>
2026-06-18 15:57:48 +0530

    Remove sqlite adapter

--- src/backend/lib/prisma.ts
+++ src/backend/lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
-
-const globalForPrismaNative2 = global as unknown as { prismaNative2: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrismaNative2.prismaNative2 ||
+  globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: ['query'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrismaNative2.prismaNative2 = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
